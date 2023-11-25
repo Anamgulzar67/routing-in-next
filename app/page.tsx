f8575a9fa8b2ad48f5ae85cc4d3984da59546b03
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-11-25 17:27:27 +0500

    got

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center w-2/3  m-auto py-32 gap-5">
     <h1 className="font-bold text-5xl">
-      Hello it's files based rounting in next app 
+      Hello it is files based rounting in next app 
     </h1>
     <p className="py-10 text-2xl">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
