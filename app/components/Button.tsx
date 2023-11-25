c7897554025dc004aea09cc24b03621d932ad260
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-11-25 15:02:20 +0500

    update

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,4 +1,29 @@
-function Button() {
-    return <button className="py-2 px-8  rounded-md bg-blue-400">Click me</button>;
-  }
-  export default Button;
+"use client"
+
+// function Button() {
+//     return <button className="py-2 px-8  rounded-md bg-blue-400">Click me</button>;
+//   }
+//   export default Button;
+
+
+
+import React, { useState } from 'react';
+
+const MyButton = () => {
+  const [buttonText, setButtonText] = useState('Click me');
+
+  const handleClick = () => {
+    // Your event handling logic here
+    // console.log('Button clicked!');
+    alert("Are you sure you want to see protfolio?");
+    // You can also update the component's state
+    setButtonText('Clicked!');
+  };
+
+  return (
+    <button onClick={handleClick} className="py-2 px-8  rounded-md bg-blue-400">
+      {buttonText}
+    </button>
+  );
+};
+export default MyButton
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import Button from "./components/Button"
+// import Button from "./components/Button";
+
+import MyButton from "./components/Button";
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center w-2/3  m-auto py-32 gap-5">
@@ -14,7 +18,7 @@ export default function Home() {
       fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
       accusantium nemo autem. Veritatis obcaecati tenetur iureobcaecati tenetur 
     </p>
-    <Button />
+    <MyButton/>
     
   </main>
   )
